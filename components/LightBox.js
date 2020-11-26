f20c7de2756d439018127aa256cd21c19a03faa9
Morgan Olsen <git@morganolsen.no>
2020-11-26 20:40:23 +0100

    add codemagic config

--- components/LightBox.js
+++ components/LightBox.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import {toggleLight, setAttribute} from '../Data';
 import Slider from 'react-native-slider';
